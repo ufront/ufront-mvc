48644b997f190719230751bad0d2cb8c8e52f7dc
Jason O'Neil <jason.oneil@gmail.com>
2013-06-13 23:36:47 +0800

    Added thx as a temporary measure until it is on the new haxelib

--- bundle.sh
+++ bundle.sh
@@ -3,4 +3,7 @@
 libname='ufront-mvc'
 rm -f "${libname}.zip"
 zip -r "${libname}.zip" haxelib.json src LICENSE.txt README.md
+cd thx
+zip -r "../${libname}.zip" src
+cd ..
 echo "Saved as ${libname}.zip"
