6a6a89214c09007e0b16a8a362c8b267b904bba9
Jason O'Neil <jason.oneil@gmail.com>
2013-07-21 12:20:52 +0800

    Further changes to auth (get it to behave same as request,response,session)

--- src/ufront/web/context/HttpContext.hx
+++ src/ufront/web/context/HttpContext.hx
@@ -1,10 +1,12 @@
 package ufront.web.context;
+import ufront.auth.IAuthUser;
 import ufront.web.url.UrlDirection;
 import thx.error.NullArgument;
 import ufront.web.url.filter.IUrlFilter;
 import thx.error.AbstractMethod;
 import ufront.web.session.FileSession;
 import ufront.web.session.IHttpSessionState;
+import ufront.auth.IAuthHandler;
 import ufront.web.url.*;
 import ufront.web.url.filter.*;
 
@@ -21,7 +23,7 @@ class HttpContext
 		`session` will be set up using `FileSession.create`, with the supplied `sessionPath`.
 
 	**/
-	public static function createWebContext(?sessionpath : String, ?request : HttpRequest, ?response : HttpResponse)
+	public static function createWebContext( ?sessionpath:String, ?request:HttpRequest, ?response:HttpResponse )
 	{
 		if(null == request)
 			request = HttpRequest.instance;
@@ -35,31 +37,34 @@ class HttpContext
 	/**
 		Create a HttpContext by explicitly supplying the request, response and session.
 
-		This is useful for mocking a context.  For real usage, it may be easier to use `createWebContext`
+		This is useful for mocking a context.  For real usage, it may be easier to use `createWebContext`.
+
+		`request` and `response` are required.  `session` and `auth` are optional, and will default to null.
 	**/
-	public function new(request : HttpRequest, response : HttpResponse, session : IHttpSessionState)
+	public function new( request:HttpRequest, response:HttpResponse, ?session:IHttpSessionState, ?auth:IAuthHandler<IAuthUser> )
 	{
 		_urlFilters = [];
-		NullArgument.throwIfNull(session);
 		NullArgument.throwIfNull(response);
 		NullArgument.throwIfNull(request);
 		this.request = request;
 		this.response = response;
 		this.session = session;
+		this.auth = auth;
 	}
 
-	public var request(get, null) : HttpRequest;
-	public var response(get, null) : HttpResponse;
-	public var session(get, null) : IHttpSessionState;
+	public var request(get, null):HttpRequest;
+	public var response(get, null):HttpResponse;
+	public var session(get, null):Null<IHttpSessionState>;
+	public var auth(get, null):Null<IAuthHandler<IAuthUser>>;
 
-	var _requestUri : String;
+	var _requestUri:String;
 	/**
 		Gets the filtered request URI. 
 
 		It uses the request uri found in the supplied `HttpRequest`, but applies the Url Filters to it.  
 		For example, if you use `PathInfoUrlFilter` to filter `index.n?path=/home/` into `/home/`, this will return the filtered result.
 	**/
-	public function getRequestUri() : String
+	public function getRequestUri():String
 	{
 		if(null == _requestUri)
 		{
@@ -77,7 +82,7 @@ class HttpContext
 		For example, if you use `PathInfoUrlFilter` this could turn `/home/` into `index.n?path=/home/`.  
 		This is useful so your code contains the simple URIs, but at runtime they are transformed into the correct form depending on the environment.
 	**/
-	public function generateUri(uri : String) : String
+	public function generateUri( uri:String ):String
 	{
 		var uriOut = VirtualUrl.parse(uri);
 		var i = _urlFilters.length - 1;
@@ -86,11 +91,11 @@ class HttpContext
 		return uriOut.toString();
 	}
 
-	var _urlFilters : Array<IUrlFilter>;
+	var _urlFilters:Array<IUrlFilter>;
 	/**
 		Add a URL filter to be used by `getRequestUri` and `generateUri`
 	**/
-	public function addUrlFilter(filter : IUrlFilter)
+	public function addUrlFilter( filter:IUrlFilter )
 	{
 		NullArgument.throwIfNull(filter);
 		_requestUri = null;
@@ -120,4 +125,5 @@ class HttpContext
 	function get_request() return request;
 	function get_response() return response;
 	function get_session() return session;
+	function get_auth() return auth;
 }
