9dd7674ef48d01f4bdf5fc5e3e9b3f6cda654656
Jason O'Neil <jason.oneil@gmail.com>
2013-07-22 14:09:22 +0800

    Added documentation. Allow instance creation on unsupported platforms.
    They will throw an error if flush() is called, but the rest will work...

--- src/ufront/web/context/HttpResponse.hx
+++ src/ufront/web/context/HttpResponse.hx
@@ -5,193 +5,282 @@ import haxe.io.Output;
 import thx.collection.HashList;
 import thx.error.NullArgument;
 import thx.error.NotImplemented;
-import haxe.ds.StringMap;
 import ufront.web.result.ActionResult;
 
 /**
-* @todo remove the singleton
-*/
+	The response that will be sent to the browser
+**/
 class HttpResponse
 {
-	public static var instance(get, null) : HttpResponse;
-	static function get_instance() : HttpResponse
+	/**
+		Get a singleton instance of the HttpResponse.
+
+		This will actually create the instance using the underlying platform implementation of this class.
+
+		Currently PHP and Neko are supported.  Other platforms will get the default implementation, which will mostly work, except for flush(), which actually writes the HTTP response output.
+	**/
+	public static var instance(get, null):HttpResponse;
+	static function get_instance():HttpResponse
 	{
 		if(null == instance)
 			#if php
 				instance = new php.ufront.web.context.HttpResponse();
 			#elseif neko
 				instance = new neko.ufront.web.context.HttpResponse();
-			#elseif nodejs
-				instance = null;
 			#else
-				throw new NotImplemented();
+				// use the default implementation.  `flush()` won't work...
+				new HttpResponse();
 			#end
 		return instance;
 	}
-
+	
+	/** "Content-type" **/
 	static inline var CONTENT_TYPE = "Content-type";
+	
+	/** "Location" **/
 	static inline var LOCATION = "Location";
+	
+	/** "text/html" **/
 	static inline var DEFAULT_CONTENT_TYPE = "text/html";
+	
+	/** "utf-8" **/
 	static inline var DEFAULT_CHARSET = "utf-8";
+	
+	/** 200 **/
 	static inline var DEFAULT_STATUS = 200;
+	
+	/** 301 **/
 	static inline var MOVED_PERMANENTLY = 301;
+	
+	/** 302 **/
 	static inline var FOUND = 302;
+	
+	/** 401 **/
 	static inline var UNAUTHORIZED = 401;
+	
+	/** 404 **/
 	static inline var NOT_FOUND = 404;
+	
+	/** 500 **/
 	static inline var INTERNAL_SERVER_ERROR = 500;
+	
+	/** Get or set the Http "Content-type" header. **/
+	public var contentType(get, set):String;
 
-	public var contentType(get, set) : String;
-	public var redirectLocation(get, set) : String;
-	public var charset : String;
-	public var status : Int;
-	public var actionContext : ActionContext;
-	public var actionResult : ActionResult;
+	/** Location to redirect to. Will add or remove a "Location" header from the HTTP headers **/
+	public var redirectLocation(get, set):String;
 
-	var _buff : StringBuf;
-	var _headers : HashList<String>;
-	var _cookies : StringMap<HttpCookie>;
+	/** Get or set the `charset` used in the HTTP "Content-type" header when the type is "text/*" **/
+	public var charset:String;
 
-	public function new()
-	{
+	/** The HTTP Response code.  See the inline static vars for common values **/
+	public var status:Int;
+
+	/** The `ActionContext` used in processing the request. Will be null until the application has processed it's dispatch **/
+	public var actionContext:ActionContext;
+
+	/** The `ActionResult` that came from processing the request. Will be null until the action has been executed. **/
+	public var actionResult:ActionResult;
+
+	var _buff:StringBuf;
+	var _headers:HashList<String>;
+	var _cookies:StringMap<HttpCookie>;
+
+	/**
+		Use DEFAULT_CHARSET, DEFAULT_STATUS.  contentType is null by default.
+	**/
+	public function new() {
 		clear();
 		contentType = null;
 		charset = DEFAULT_CHARSET;
 		status = DEFAULT_STATUS;
 	}
 
-	public function flush() : Void{}
-	public function clear()
-	{
+	/**
+		Write the output to the response.  
+
+		This includes writing the cookies, the HttpHeaders and then the content.
+
+		This is an abstract method, it is implemented differently on each platform.
+
+		It will throw NotImplemented() unless a subclass overrides this method.
+	**/
+	public function flush():Void { throw new NotImplemented(); }
+
+	/**
+		Will clear existing headers, cookies and content
+	**/
+	public function clear() {
 		clearCookies();
 		clearHeaders();
 		clearContent();
 	}
 
-	public function clearCookies()
-	{
+	/**
+		Clear any cookies set in this response so far.
+	**/
+	public function clearCookies() {
 		_cookies = new StringMap();
 	}
 
-	public function clearContent()
-	{
+	/**
+		Clear the content set in this response so far.
+	**/
+	public function clearContent() {
 		_buff = new StringBuf();
 	}
 
-	public function clearHeaders()
-	{
+	/**
+		Clear the headers set in this response so far.
+	**/
+	public function clearHeaders() {
 		_headers = new HashList();
 	}
 
-	public function write(s : String)
-	{
-		if(null != s)
-			_buff.add(s);
+	/**
+		Write a string to the HTTP response
+	**/
+	public function write( s:String ) {
+		if( null!=s )
+			_buff.add( s );
 	}
 
-	public function writeChar(c : Int)
-	{
-		_buff.addChar(c);
+	/**
+		Write a single character to the HTTP response
+	**/
+	public function writeChar( c:Int ) {
+		_buff.addChar( c );
 	}
 
-	public function writeBytes(b : Bytes, pos : Int, len : Int)
-	{
-		_buff.add(b.readString(pos, len));
+	/**
+		Write a number of bytes to the HTTP response
+	**/
+	public function writeBytes( b:Bytes, pos:Int, len:Int ) {
+		_buff.add( b.readString(pos, len) );
 	}
 
-	public function setHeader(name : String, value : String)
-	{
-		NullArgument.throwIfNull(name);
-		NullArgument.throwIfNull(value);
-		_headers.set(name, value);
+	/**
+		Set a HTTP Header on the response
+	**/
+	public function setHeader( name:String, value:String ) {
+		NullArgument.throwIfNull( name );
+		NullArgument.throwIfNull( value );
+		_headers.set( name, value );
 	}
 
-	public function setCookie(cookie : HttpCookie)
-	{
-		_cookies.set(cookie.name, cookie);
+	/**
+		Set a HTTP Cookie on the response
+	**/
+	public function setCookie( cookie:HttpCookie ) {
+		_cookies.set( cookie.name, cookie );
 	}
 
-	public function getBuffer()
-	{
+	/**
+		Get the current content output (String) of this response
+	**/
+	public function getBuffer():String {
 		return _buff.toString();
 	}
 
-	public function getCookies()
-	{
+	/**
+		Get the `StringMap` of Cookies set in this response
+	**/
+	public function getCookies() {
 		return _cookies;
 	}
 
-	public function getHeaders()
-	{
+	/**
+		Get the `HashList` of HTTP headers set in this response.
+
+		A HashList is basically a StringMap, but it preserves the order of the items (headers in this case)
+	**/
+	public function getHeaders() {
 		return _headers;
 	}
 
-	public function redirect(url : String)
-	{
+	/**
+		Set the HTTP Response Code to `FOUND` (302) and set the `redirectLocation`, which will set the `Location` HTTP header.
+
+		If URL is null, the `Location` header will be removed.
+	**/
+	public function redirect( url:String ) {
 		status = FOUND;
 		redirectLocation = url;
 	}
 
-	public function setOk()
-	{
+	/**
+		Set the HTTP Response Code to `DEFAULT_STATUS` (200)
+	**/
+	public function setOk() {
 		status = DEFAULT_STATUS;
 	}
 
-	public function setUnauthorized()
-	{
+	/**
+		Set the HTTP Response Code to `UNAUTHORIZED` (401)
+	**/
+	public function setUnauthorized() {
 		status = UNAUTHORIZED;
 	}
 
-	public function setNotFound()
-	{
+	/**
+		Set the HTTP Response Code to `NOT_FOUND` (404)
+	**/
+	public function setNotFound() {
 		status = NOT_FOUND;
 	}
 
-	public function setInternalError()
-	{
+	/**
+		Set the HTTP Response Code to `INTERNAL_SERVER_ERROR` (500)
+	**/
+	public function setInternalError() {
 		status = INTERNAL_SERVER_ERROR;
 	}
 
-	public function permanentRedirect(url : String)
-	{
+	/**
+		Set the HTTP Response Code to `MOVED_PERMANENTLY` (301) and set the `redirectLocation`, which will set the `Location` HTTP header.
+
+		If URL is null, the `Location` header will be removed.
+	**/
+	public function permanentRedirect( url:String ) {
 		status = MOVED_PERMANENTLY;
 		redirectLocation = url;
 	}
 
-	public function isRedirect()
-	{
-		return Math.floor(status / 100) == 3;
+	/**
+		A shortcut to tell whether the current status indicates this response is a redirect (true) or not (false)
+	**/
+	public function isRedirect() {
+		return Math.floor( status/100 ) == 3;
 	}
 
-	public function isPermanentRedirect()
-	{
+	/**
+		A shortcut to tell whether the current status indicates this response is a permanent redirect (true) or not (false)
+	**/
+	public function isPermanentRedirect() {
 		return status == MOVED_PERMANENTLY;
 	}
 
-	function get_contentType()
-	{
-		return _headers.get(CONTENT_TYPE);
+	function get_contentType() {
+		return _headers.get( CONTENT_TYPE );
 	}
 
-	function set_contentType(v : String)
-	{
-		if (null == v)
-			_headers.set(CONTENT_TYPE, DEFAULT_CONTENT_TYPE)
+	function set_contentType(v:String) {
+		if ( null==v )
+			_headers.set( CONTENT_TYPE, DEFAULT_CONTENT_TYPE )
 		else
-			_headers.set(CONTENT_TYPE, v);
+			_headers.set( CONTENT_TYPE, v );
 		return v;
 	}
 
-	function get_redirectLocation()
-	{
-		return _headers.get(LOCATION);
+	function get_redirectLocation() {
+		return _headers.get( LOCATION );
 	}
 
-	function set_redirectLocation(v : String)
-	{
-		if (null == v)
-			_headers.remove(LOCATION)
+	function set_redirectLocation( v:String ) {
+		if ( null==v )
+			_headers.remove( LOCATION )
 		else
-			_headers.set(LOCATION, v);
+			_headers.set( LOCATION, v );
 		return v;
 	}
 }
