ac6f55001043e3a368d80d6b739d6fc7c479d54f
Jason O'Neil <jason.oneil@gmail.com>
2013-07-30 21:21:02 +0800

    Include basic Controller.  Doesn't do much yet

---
+++ src/ufront/web/Controller.hx
@@ -0,0 +1,28 @@
+package ufront.web;
+
+import ufront.web.context.ActionContext;
+
+/**
+	A simple base controller class.  
+	It's main function is an autobuild macro, which will be used to provide some helpers in future.  
+	It also has an empty constructor and a `context:ActionContext` property.  See below for details.
+**/
+// @:autoBuild()
+@:keep
+class Controller
+{
+	/** 
+		The Action Context.  
+		This is not set automatically, it is up to the DispatchModule or similar to pass the context to the controller.  
+		This has a setter that you can override if you want to perform any initialisation logic when the context is set.
+	**/
+	@:isVar
+	public var context(default,set):Null<ActionContext>;
+	function set_context(v) return context=v;
+
+	/**
+		Empty constructor.  
+		Included so that you don't need a constructor in your container
+	**/
+	public function new() {}
+}
