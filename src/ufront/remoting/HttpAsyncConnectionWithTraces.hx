26b9f6fd23bb05d449f15f97ec6361e7b7df5b5c
Jason O'Neil <jason.oneil@gmail.com>
2013-09-18 19:30:40 +0800

    Merged ufront-remoting into ufront-mvc
    With the better integration of remoting into UfrontApplication,
    these libraries would have been dependant on each other, which is
    messy.  Better to just merge the libraries if you need them both anyway

---
+++ src/ufront/remoting/HttpAsyncConnectionWithTraces.hx
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C)2005-2012 Haxe Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+package ufront.remoting;
+
+import haxe.remoting.AsyncConnection;
+import haxe.remoting.HttpAsyncConnection;
+import ufront.log.Message;
+import haxe.CallStack;
+using StringTools;
+
+
+/** 
+	Extension of class that allows traces to be sent.  
+	On the server side, just make sure you write to `Lib.printLn();` before `handleRequest(context)` is called.
+	
+	This all relies on Haxe serialised data always being one line.
+	
+	A line beginning with hxr is the remoting call, a line beginning with hxt is a trace
+		
+	Any other line will throw an "Invalid response" error.
+**/
+class HttpAsyncConnectionWithTraces extends HttpAsyncConnection 
+{
+	override public function resolve( name ) : AsyncConnection {
+		var c = new HttpAsyncConnectionWithTraces(__data,__path.copy());
+		c.__path.push(name);
+		return c;
+	}
+
+	// Code mostly copied from super class, but the onData() response has been modified to output traces
+	override public function call( params : Array<Dynamic>, ?onResult : Dynamic -> Void ) {
+
+		// Track the call-stack in case there's an error (not possible on safari/IE currently)
+		var callStack = try CallStack.callStack() catch(e:Dynamic) [Module('Stack traces not supported in this browser...')];
+
+		// Set up the remoting call
+		var h = new haxe.Http(__data.url);
+		#if (neko && no_remoting_shutdown)
+			h.noShutdown = true;
+		#end
+		var s = new haxe.Serializer();
+		s.serialize(__path);
+		s.serialize(params);
+		h.setHeader("X-Haxe-Remoting","1");
+		h.setParameter("__x",s.toString());
+
+		// Set up the remoting data/error callbacks
+		var error = __data.error;
+		h.onData = function( response : String ) {
+			var ok = true;
+			var ret = null;
+			var stack:String = null;
+			try {
+				var hxrFound = false;
+				for (line in response.split('\n')) {
+					switch (line.substr(0,3)) {
+						case "hxr":
+							var s = new haxe.Unserializer(line.substr(3));
+							ret = s.unserialize();
+							hxrFound = true;
+						case "hxt":
+							var s = new haxe.Unserializer(line.substr(3));
+							var m:Message = s.unserialize();
+							#if js 
+								var extras = 
+									if ( m.pos!=null && m.pos.customParams!=null ) " "+m.pos.customParams.join(" ")
+									else "";
+								var msg = '[R]${m.pos.className}.${m.pos.methodName}(${m.pos.lineNumber}): ${m.msg}$extras';
+								var c = js.Browser.window.console;
+								switch m.type {
+									case Trace: c.log( msg );
+									case Log: c.info( msg );
+									case Warning: c.warn( msg );
+									case Error: c.error( msg );
+								}
+							#else
+								m.pos.fileName="[R]"+m.pos.fileName;
+								haxe.Log.trace('[${m.type}]${m.msg}', m.pos);
+							#end
+						case "hxs":
+							var s = new haxe.Unserializer(line.substr(3));
+							stack = s.unserialize();
+						case "hxe":
+							// Unserializing an exception will throw it
+							var s = new haxe.Unserializer(line.substr(3));
+							ret = s.unserialize();
+						default:
+							throw "Invalid line in response : '"+line+"'";
+					}
+				}
+				if (hxrFound == false) throw "Invalid response, no hxr remoting line was found: " + response;
+			} 
+			catch( err : Dynamic ) 
+			{
+
+				// Pass the error to the error handler
+				ok = false;
+				ret = null;
+
+				error({ err: err, stack: stack });
+			}
+
+			if( ok && onResult != null )
+			{
+				try onResult(ret) catch (e:Dynamic) {
+					trace ('  Error: $e');
+					#if debug 
+						var cs = CallStack.toString(callStack);
+						trace ('  CallStack: $cs');
+						trace ("  Launching Debugger...");
+						#if js 
+							js.Lib.debug();
+						#end
+					#end
+				}
+			}
+		};
+		h.onError = error;
+
+		// Run the request
+		h.request(true);
+	}
+
+	public static function urlConnect( url : String ) {
+		return new HttpAsyncConnectionWithTraces({ url : url, error : function(e) throw e },[]);
+	}
+
+}
---
+++ src/ufront/remoting/RemotingModule.hx
@@ -0,0 +1,149 @@
+package ufront.remoting;
+
+import haxe.remoting.Context;
+import haxe.Serializer;
+import haxe.Unserializer;
+import haxe.CallStack;
+import hxevents.Async;
+import ufront.module.IHttpModule;
+import ufront.application.HttpApplication;
+import ufront.web.error.*;
+import ufront.web.result.*;
+import ufront.web.context.*;
+import minject.Injector;
+import ufront.application.*;
+import ufront.auth.*;
+
+/**
+	Checks if a request is a remoting request and processes accordingly.
+
+	It looks for the "X-Haxe-Remoting" remoting header to check if this is a remoting call - the path/URL used does not matter.
+	If it is a remoting call, it will process it accordingly and return a remoting result (basically serialized Haxe values).
+	Traces are also serialized and sent to the client.
+
+	@author Jason O'Neil
+**/
+class RemotingModule implements IHttpModule
+{
+	var injector:Injector;
+	var apis:List<Class<RemotingApiContext>>;
+	var app:HttpApplication;
+
+	/** Construct a new RemotingModule, optionally adding an API to the remoting Context. **/
+	public function new() {
+		apis = new List();
+	}
+
+	/** Expose a RemotingApiContext to the request **/
+	public inline function loadApi( remotingApiContext:Class<RemotingApiContext> ) {
+		apis.push( remotingApiContext );
+	}
+
+	/** Initializes a module and prepares it to handle remoting requests. */
+	public function init( application:HttpApplication ):Void {
+		app = application;
+		injector = 
+			if (Std.is(app, UfrontApplication)) cast(app,UfrontApplication).remotingInjector
+			else app.appInjector;
+		app.onPostResolveRequestCache.addAsync( executeRemoting );
+	}
+
+	/** Disposes of the resources (other than memory) that are used by the module. */
+	public function dispose():Void {
+		injector = null;
+		apis = null;
+	}
+
+	function executeRemoting( httpContext:HttpContext, async:Async ) {
+		if ( httpContext.request.clientHeaders.exists("X-Haxe-Remoting") ) {
+			
+			// Set up the injector
+			var requestInjector = injector.createChildInjector();
+			requestInjector.mapValue( IAuthHandler, httpContext.auth );
+			requestInjector.mapValue( Array, httpContext.messages, "messages" );
+
+			// Map the specific implementations for auth and session
+			requestInjector.mapValue( Type.getClass( httpContext.session ), httpContext.session );
+			requestInjector.mapValue( Type.getClass( httpContext.auth ), httpContext.auth );
+
+			// Set up the context
+			var context = new Context();
+			for (api in apis) {
+				var apiContext = requestInjector.instantiate( api );
+				for (fieldName in Reflect.fields(apiContext)) {
+					var o = Reflect.field(apiContext, fieldName);
+					if (Reflect.isObject(o))
+						context.addObject(fieldName, o);
+				}
+			}
+			
+			// Execute the request
+			var r = httpContext.response;
+			var remotingResponse:String;
+			try {
+				// Check the '__x' parameter is present
+				var params = httpContext.request.params;
+				if ( !params.exists("__x") ) 
+					throw 'Remoting call did not have parameter `__x` which describes which API call to make.  Aborting';
+				
+				// Execute the response ... TODO... can we make this support async?
+				remotingResponse = processRequest( params["__x"], context, httpContext );
+				r.setOk();
+			}
+			catch ( e:Dynamic ) {
+				// Don't use the `async.error` handler and the ErrorModule, rather, send the error over the remoting protocol.
+				remotingResponse = remotingError( e, httpContext );
+				r.setInternalError();
+			}
+
+			// Set the response
+			r.contentType = "application/x-haxe-remoting";
+			r.clearContent();
+			r.write( remotingResponse );
+
+			// Mark this request as complete so further events don't fire
+			httpContext.completed = true;
+
+			// Run the log functions asynchronously, then complete.  In the event of an error, complete anyway (our request worked, just not the logging)
+			var onError = function (e) { async.completed(); }
+			app.onLogRequest.dispatch( httpContext, function () {
+				app.onPostLogRequest.dispatch( httpContext, function () {
+					async.completed();
+				}, onError );
+			}, onError );
+		}
+		else async.completed(); // Not a remoting call
+	}
+
+	function processRequest( requestData:String, ctx:Context, httpContext:HttpContext ):String {
+		var u = new Unserializer( requestData );
+		var path:Array<String> = u.unserialize();
+		var args:Array<String> = u.unserialize();
+		httpContext.ufTrace('Remoting call ${path.join(".")}(${args.join(", ")})');
+		var data = ctx.call( path, args );
+		var s = new Serializer();
+		s.serialize( data );
+		return "hxr" + s.toString();
+	}
+
+	function remotingError( e:Dynamic, httpContext:HttpContext ):String {
+		// Log the error
+		httpContext.ufError( e );
+
+		// Serialize the exception
+		var s = new haxe.Serializer();
+		s.serializeException(e);
+		var serializedException = "hxe" + s.toString();
+
+		#if debug
+			// Serialize the stack trace
+			var exceptionStack = CallStack.toString( CallStack.exceptionStack() );
+			var serializedStack = "hxs" + Serializer.run( exceptionStack );
+
+			return '$serializedStack\n$serializedException';
+		#else
+			return '$serializedException';
+		#end
+
+	}
+}
