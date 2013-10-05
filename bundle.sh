0ee8457214245a3b6b8b28e6fe664b76247be6dc
Jason O'Neil <jason.oneil@gmail.com>
2013-10-05 17:54:06 +0800

    Updated bundle to reflect that thx is now in main src/ directory

--- bundle.sh
+++ bundle.sh
@@ -3,7 +3,4 @@
 libname='ufront-mvc'
 rm -f "${libname}.zip"
 zip -r "${libname}.zip" haxelib.json src LICENSE.txt README.md
-cd thx
-zip -r "../${libname}.zip" src
-cd ..
 echo "Saved as ${libname}.zip"
