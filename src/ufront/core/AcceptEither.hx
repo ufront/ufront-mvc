3ac9f6952116d627355b06adefc2a01c222e3727
Jason O'Neil <jason.oneil@gmail.com>
2014-05-21 10:54:40 +0800

    Add AcceptEither, change DetoxResult
    AcceptEither is an abstract that lets you accept one of two value types in your function arguments, and
    then uses `tink.core.Either`to give you a type safe way to handle both cases.
    
    DetoxResult now works directly with widgets, and uses `mapData()` rather than implementing a
    similar reflection strategy.  In time the aim here to have a client side version where the view layout
    can persist between requests, updating individual content/properties only as required.

---
+++ src/ufront/core/AcceptEither.hx
@@ -0,0 +1,35 @@
+package ufront.core;
+
+import tink.CoreApi;
+/**
+	`AcceptEither`, a way to accept either one type or another, without resorting to “Dynamic”, and still have the compiler type-check everything and make sure you correctly handle every situation.
+
+	The abstract has implicit casts for both A and B, so you can easily receive either value.
+	You can switch on `acceptEither.type` to extract the value depending on the type.
+
+	Example:
+
+	```
+	function divideInHalf( in:AcceptEither<Int,String> ):Float {
+		return switch in {
+			case Left(i): i / 2;
+			case Right(str): Std.parseFloat(str) / 2;
+		}
+	}
+	```
+**/
+abstract AcceptEither<A,B> (Either<A,B>) {
+
+	inline function new( e:Either<A,B> ) this = e;
+
+	/** Get the value regardless of type. **/
+	public var value(get,never):Dynamic;
+
+	/** Return an enum to let you choose either option A or B **/
+	public var type(get,never):Either<A,B>;
+
+	inline function get_value() switch this { case Left(v) | Right(v): return v; }
+	@:to inline function get_type() return this;
+	@:from static function fromA( v:A ):AcceptEither<A,B> return new AcceptEither( Left(v) );
+	@:from static function fromB( v:B ):AcceptEither<A,B> return new AcceptEither( Right(v) );
+}
--- src/ufront/web/result/DetoxResult.hx
+++ src/ufront/web/result/DetoxResult.hx
@@ -7,40 +7,97 @@ import ufront.web.context.ActionContext;
 import dtx.widget.Widget;
 import dtx.layout.IDetoxLayout;
 import ufront.core.Sync;
+import ufront.view.TemplateData;
+import ufront.core.AcceptEither;
 using Detox;
 
-/** Represents a class that is used to send JSON-formatted content to the response. */
-class DetoxResult<W:DOMCollection> extends ActionResult
-{
-	public var content : W;
-	public var title : Null<String>;
-	public var layout : Null<IDetoxLayout>;
+/**
+	A DetoxResult is an ActionResult that uses a `dtx.DOMCollection` for it's layout, and applies various template data to it using `widget.mapData` (which itself uses `Reflect.setProperty`).
 
-	public function new( content:W, ?title:String, ?layout:IDetoxLayout ) {
-		NullArgument.throwIfNull(content);
-		this.content = content;
-		this.title = title;
-		this.layout = layout;
+	Once ufront supports client-side apps, the intention is for a DetoxResult to share a layout between requests, just replacing the data in the layout, for smooth one-page apps.
+
+	Currently only server side interaction is supported - it creates the layout, and then populates it with the data.
+
+	Please note that using providing the `data` parameter will use Reflection to set the properties on your layout, and so type safety is not guaranteed.
+	You can access `layout` to get a typed layout to set your properties on directly for maximum safety.
+
+	```
+	// Inline, use TemplateData, not type checked.
+	return new DetoxResult( StaffLayout, { title: "Welcome!", view: myViewWidget } );
+
+	// Or, fully type checked
+	var layout = new StaffLayout();
+	layout.title = "Welcome!";
+	layout.view = myViewWidget;
+	return new DetoxResult( layout );
+	```
+**/
+class DetoxResult<W:Widget> extends ActionResult {
+
+	/** The default HTTP content type to use for responses if none is specified. Default value is `text/html`. **/
+	public static var defaultContentType = "text/html";
+
+	/** The default DOCTYPE to print for responses if none is specified. Default value is `<!DOCTYPE html>`. **/
+	public static var defaultDocType = "<!DOCTYPE html>";
+
+	/** The layout used for the current request. This is instantiated during the constructor. Readonly. **/
+	public var layout(default,null):W;
+
+	/**
+		The template data to apply to the layout.
+		This is initialized in the constructor.
+		If no data is provided, an empty TemplateData collection will be created.
+
+		For type safe access consider setting properties of `layout` directly.
+		Each piece of data will be applied using `Reflect.setProperty(layout,key,value)`.
+
+		The data will be applied to the layout during `executeResult`.
+
+		Readonly.
+	**/
+	public var data(default,null):TemplateData;
+
+	/** The HTTP content type to use for this request. If not specified before `executeResult()` is called, then it will be set to `defaultContentType`. **/
+	public var contentType:String;
+
+	/** The DOCTYPE to use for this request. If not specified before `executeResult()` is called, then it will be set to `defaultDocType`. **/
+	public var docType:String;
+
+	/**
+		Create a new DetoxResult with the specified layout.
+
+		@param layoutToUse Either a widget that is ready to use, or the class of widget to create. The class must be able to be instantiated without having arguments provided to the constructor. It will be constructed immediately.
+		@param (optional) data Any initial data to be set on the widget. These will not be applied until `executeResult` is called. If not supplied, an empty TemplateData collection will be created.
+		@param (optional) docType An initial value for `docType`.
+		@param (optional) docType An initial value for `contentType`.
+	**/
+	public function new( layoutToUse:AcceptEither<W,Class<W>>, ?data:TemplateData, ?contentType:String, ?docType:String ) {
+		this.layout = switch layoutToUse.type {
+			case Left(l): l;
+			case Right(cl): Type.createInstance( cl, [] );
+		}
+		this.data = ( data!=null ) ? data : {};
+		this.contentType = contentType;
+		this.docType = docType;
 	}
 
+	/**
+		Execute the result.  This will:
+
+		- Apply the template data to the layout using `layout.mapData`.
+		- Set the HttpResponse content type using either the provided `contentType` or the `defaultContentType`.
+		- Write the doctype to the HttpResponse using either the provided `docType` or the `defaultDocType`.
+		- Write the html to the output using `layout.html()`.
+
+		This method performs synchronously.
+	**/
 	override function executeResult( actionContext:ActionContext ) {
-		// If layout is null, get a default one...
-		if (layout==null) layout = getDefaultLayout();
+		layout.mapData( data );
 
-		layout.contentContainer.empty().append( content );
-		if (title!=null) layout.title = title;
-
-		actionContext.response.contentType = "text/html";
-		actionContext.response.write( '<!DOCTYPE html>' + layout.document.html() );
+		var dt = (docType!=null) ? docType : defaultDocType;
+		actionContext.response.contentType = (contentType!=null) ? contentType : defaultContentType;
+		actionContext.response.write( dt + layout.html() );
 
 		return Sync.success();
-	}
-
-	// TODO: move somewhere else?
-	public static var defaultLayoutFactory:Void->IDetoxLayout;
-	static function getDefaultLayout() {
-		return 
-			if ( defaultLayoutFactory!=null ) defaultLayoutFactory();
-			else new DefaultDetoxLayout();
 	}
 }
