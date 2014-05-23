9e2692b4fd5d99e3453ce45f7ebab75632fb2834
Jason O'Neil <jason.oneil@gmail.com>
2014-05-23 08:35:21 +0800

    Fixes and documentation for TemplateData class

--- src/ufront/view/TemplateData.hx
+++ src/ufront/view/TemplateData.hx
@@ -11,62 +11,155 @@ using StringTools;
 	- Map<String,Dynamic>
 	- Iterable<TemplateData>
 	
+	These methods are provided to access or modify the contents of the template data:
+
+	- `get()`
+	- `set()`
+	- `setObject()`
+	- `setMap()`
+
+	Array access is also provided for getting / setting data.
+
 	No string conversion or escaping happens at this level, that is up to the templating engine.
 **/
 abstract TemplateData({}) {
 	
-	inline function new( obj:{} ) 
+	inline function new( obj:{} )
 		this = obj;
 
-	/** Convert into a `Dynamic<String>` anonymous object **/
-	@:to public function toObject():{}
+	/**
+		Convert into a `Dynamic<Dynamic>` anonymous object.
+		Please note this is not an implicit `@:to` cast, because the resulting type would match too many false positives.
+		To use this cast call `templateData.toObject()` explicitly.
+	**/
+	public inline function toObject():Dynamic<Dynamic>
 		return this;
 
-	/** Convert into a regular map **/
-	@:to public inline function toMap():Map<String,Dynamic> {
+	/**
+		Convert into a `Map<String,Dynamic>`.
+		This is also available as an implicit `@:to` cast.
+	**/
+	@:to public function toMap():Map<String,Dynamic> {
 		var ret = new Map<String,Dynamic>();
 		for ( k in Reflect.fields(this) ) ret[k] = Reflect.field( this, k );
 		return ret;
 	}
 
-	/** Get a value from the template data.  This is also used for array access. **/
-	@:arrayAccess public inline function get( key:String ) return Reflect.field( this, key );
+	/**
+		Convert into a `StringMap<Dynamic>`.
+		This is also available as an implicit `@:to` cast.
+	**/
+	@:to public inline function toStringMap():Map<String,Dynamic> {
+		return toMap();
+	}
+
+	/**
+		Get a value from the template data.
+		This is also used for array access: `templateData['name']` is the same as `templateData.get('name')`.
+
+		@param key The name of the value to retrieve.
+		@return The value, or null if it was not available.
+	**/
+	@:arrayAccess public inline function get( key:String ):Null<Dynamic> return Reflect.field( this, key );
 	
-	/** Set a value on the template data.  The value will be run through `StringTools.htmlEscape`.  This is also used for array access setting of values. **/
-	@:arrayAccess public inline function set( key:String, val:Dynamic ):TemplateData {
+	/**
+		Set a value on the template data.
+
+		Please note array setters are also available, but they use the private `array_set` method which returns the value, rather than the TemplateData object.
+
+		@param key The name of the key to set.
+		@param val The value to set.
+		@return The same TemplateData so that method chaining is enabled.
+	**/
+	public function set( key:String, val:Dynamic ):TemplateData {
 		Reflect.setField( this, key, val );
-		return new TemplateData(this);
+		return new TemplateData( this );
 	}
 
-	/** Set many unescaped values from a StringMap **/
-	public function setMap( d:Map<String,Dynamic> ):TemplateData {
-		for ( k in d.keys() ) set( k, d.get(k) );
-		return new TemplateData(this);
+	/** Array access setter. **/
+	@:arrayAccess function array_set<T>( key:String, val:T ):T {
+		Reflect.setField( this, key, val );
+		return val;
 	}
 
-	/** Set many unescaped values from an anonymous object **/
+	/**
+		Set many values from a `Map<String,Dynamic>`
+
+		`templateData.set(key,map[key])` will be called for each pair in the map.
+
+		@param map The map data to set.
+		@return The same TemplateData so that method chaining is enabled.
+	**/
+	public function setMap<T>( map:Map<String,T> ):TemplateData {
+		for ( k in map.keys() ) {
+			set( k, map[k] );
+		}
+		return new TemplateData( this );
+	}
+
+	/**
+		Set many values from a `Map<String,Dynamic>`
+
+		`templateData.set(fieldName,fieldValue)` will be called for each field on the object.
+
+		Reflect is used with `fields` and `field` to read the value of every field.
+
+		@param map The data object to set.
+		@return The same TemplateData so that method chaining is enabled.
+	**/
 	public function setObject( d:{} ):TemplateData {
 		for ( k in Reflect.fields(d) ) set( k, Reflect.field(d,k) );
-		return new TemplateData(this);
+		return new TemplateData( this );
 	}
 
 	/** from casts **/
 
-	@:from public static function fromObject( d:{} ):TemplateData {
-		return new TemplateData( d );
-	}
-
-	@:from public static function fromMap( d:Map<String,Dynamic> ):TemplateData {
+	/**
+		Automatically cast from a `Map<String,Dynamic>` into a TemplateData.
+	**/
+	@:from public static function fromMap<T>( d:Map<String,T> ):TemplateData {
 		var m:TemplateData = new TemplateData( {} );
 		m.setMap( d );
 		return m;
 	}
 
+	/**
+		Automatically cast from a `StringMap<Dynamic>` into a TemplateData.
+	**/
+	@:from public static inline function fromStringMap<T>( d:StringMap<T> ):TemplateData {
+		return fromMap( d );
+	}
+
+	/**
+		Automatically cast from a `Iterable<TemplateData>` into a combined `TemplateData.
+		Values will be added in order, and later values with the same name as an earlier value will override the earlier value.
+		If the iterable is empty, the resulting TemplateData will contain no properties.
+		If an individual item is a StringMap, it will be added with `setMap`, otherwise it will be added with `setObject`.
+
+		@param dataSets The collection of TemplateData objects to iterate over.
+		@return The same TemplateData so that method chaining is enabled.
+	**/
 	@:from public static function fromMany( dataSets:Iterable<TemplateData> ):TemplateData {
-		var m:TemplateData = new TemplateData( {} );
+		var combined:TemplateData = new TemplateData( {} );
 		for ( d in dataSets ) {
-			m.setObject( d.toObject() );
+			if ( Std.is(d,StringMap) ) {
+				var map:StringMap<Dynamic> = cast d;
+				combined.setMap( (map:StringMap<Dynamic>) );
+			}
+			else {
+				var obj:Dynamic = d;
+				combined.setObject( obj );
+			}
 		}
-		return m;
+		return combined;
+	}
+
+	/**
+		Automatically cast from an object to a TemplateData.
+		This is a no-op - the object will be used as is.
+		This cast comes last in the code so it should be used only if none of the other casts were utilised.
+	**/
+	@:from public static inline function fromObject( d:{} ):TemplateData {
+		return new TemplateData( d );
 	}
 }
--- test/ufront/view/TemplateDataTest.hx
+++ test/ufront/view/TemplateDataTest.hx
@@ -4,6 +4,7 @@ import massive.munit.util.Timer;
 import massive.munit.Assert;
 import massive.munit.async.AsyncFactory;
 import ufront.view.TemplateData;
+import haxe.ds.StringMap;
 
 class TemplateDataTest 
 {
@@ -35,8 +36,170 @@ class TemplateDataTest
 	}
 	
 	@Test
-	public function testExample():Void
+	public function toObject():Void
 	{
-		Assert.isTrue(false);
+		var data:TemplateData = { name: "jason", age: 26 };
+
+		var obj = data.toObject();
+		Assert.areEqual( "jason", obj.name );
+	}
+
+	@Test
+	public function toMap():Void
+	{
+		var data:TemplateData = { name: "jason", age: 26 };
+
+		var map = data.toMap();
+		Assert.areEqual( "jason", map["name"] );
+
+		var map2:Map<String,Dynamic> = data;
+		Assert.areEqual( 26, map2["age"] );
+	}
+
+	@Test
+	public function toStringMap():Void
+	{
+		var data:TemplateData = { name: "jason", age: 26 };
+
+		var map = data.toStringMap();
+		Assert.areEqual( "jason", map["name"] );
+
+		var map2:StringMap<Dynamic> = data;
+		Assert.areEqual( 26, map2.get("age") );
+	}
+
+	@Test
+	public function get():Void
+	{
+		var data:TemplateData = { name: "jason", age: 26 };
+		Assert.areEqual( "jason", data.get("name") );
+		Assert.areEqual( "jason", data["name"] );
+		Assert.areEqual( 26, data.get("age") );
+		Assert.areEqual( null, data.get("language") );
+	}
+
+	@Test
+	public function set():Void
+	{
+		var data:TemplateData = { name: "jason", age: 26 };
+		Assert.areEqual( 2, Reflect.fields(data).length );
+
+		data.set( 'language', 'Haxe' );
+		Assert.areEqual( 3, Reflect.fields(data).length );
+		Assert.areEqual( "Haxe", data.get("language") );
+
+		data['name'] = "Franco";
+		Assert.areEqual( 3, Reflect.fields(data).length );
+		Assert.areEqual( "Franco", data.get("name") );
+	}
+
+	@Test
+	public function setMap():Void
+	{
+		var data:TemplateData = { name: "jason", age: 26 };
+		data.setMap([ "name" => "Franco", "language" => "Haxe" ]);
+		Assert.areEqual( 3, Reflect.fields(data).length );
+		Assert.areEqual( "Franco", data["name"] );
+		Assert.areEqual( 26, data["age"] );
+		Assert.areEqual( "Haxe", data["language"] );
+	}
+
+	@Test
+	public function setObject():Void
+	{
+		var data:TemplateData = { name: "jason", age: 26 };
+		data.setObject({ name: "jason", language: "Haxe" });
+		Assert.areEqual( 3, Reflect.fields(data).length );
+		Assert.areEqual( "jason", data["name"] );
+		Assert.areEqual( 26, data["age"] );
+		Assert.areEqual( "Haxe", data["language"] );
+	}
+
+	@Test
+	public function fromObject():Void
+	{
+		var emptyData:TemplateData = {};
+		Assert.areEqual( 0, Reflect.fields(emptyData).length );
+
+		var data1 = TemplateData.fromObject({ "name": "jason" });
+		Assert.areEqual( 1, Reflect.fields(data1).length );
+		Assert.areEqual( "jason", data1["name"] );
+
+		var data2 = TemplateData.fromObject({ name: "jason", age: 26 });
+		Assert.areEqual( 2, Reflect.fields(data2).length );
+		Assert.areEqual( "jason", data2["name"] );
+		Assert.areEqual( 26, data2["age"] );
+
+		var data3 = TemplateData.fromObject({ language: "Haxe", targets: ["neko","php","js"] });
+		Assert.areEqual( 2, Reflect.fields(data3).length );
+		Assert.areEqual( "Haxe", data3["language"] );
+		Assert.areEqual( 3, data3["targets"].length );
+		Assert.areEqual( "php", data3["targets"][1] );
+
+		// Test implicit cast
+		var data4:TemplateData = { language: "Haxe", targets: ["neko","php","js"] };
+		Assert.areEqual( 2, Reflect.fields(data4).length );
+		Assert.areEqual( "Haxe", data4["language"] );
+		Assert.areEqual( 3, data4["targets"].length );
+		Assert.areEqual( "php", data4["targets"][1] );
+	}
+
+	@Test
+	public function fromMap():Void
+	{
+		var map = [ "name" => "jason", "language" => "Haxe" ];
+		var data1 = TemplateData.fromMap(map);
+		Assert.areEqual( 2, Reflect.fields(data1).length );
+		Assert.areEqual( "jason", data1["name"] );
+		Assert.areEqual( "Haxe", data1["language"] );
+
+		var data2:TemplateData = map;
+		Assert.areEqual( 2, Reflect.fields(data2).length );
+		Assert.areEqual( "jason", data2["name"] );
+		Assert.areEqual( "Haxe", data2["language"] );
+	}
+
+	@Test
+	public function fromStringMap():Void
+	{
+		var stringMap:StringMap<String> = [ "name" => "jason", "language" => "Haxe" ];
+		var data:TemplateData = stringMap;
+		Assert.areEqual( 2, Reflect.fields(data).length );
+		Assert.areEqual( "jason", data["name"] );
+		Assert.areEqual( "Haxe", data["language"] );
+	}
+
+	@Test
+	public function fromMany():Void
+	{
+		var emptyData:TemplateData = [];
+		Assert.areEqual( 0, Reflect.fields(emptyData).length );
+
+		var dataObj:TemplateData = { name: "jason", age: 26 };
+		var dataMap:TemplateData = [ "name"=>"Franco", "language"=>"Haxe" ];
+
+		var combined = TemplateData.fromMany([ dataObj, dataMap ]);
+		Assert.areEqual( 3, Reflect.fields(combined).length );
+		Assert.areEqual( "Franco", combined["name"] );
+		Assert.areEqual( 26, combined["age"] );
+		Assert.areEqual( "Haxe", combined["language"] );
+
+		var combined1:TemplateData = [ dataObj, dataMap ];
+		Assert.areEqual( 3, Reflect.fields(combined1).length );
+		Assert.areEqual( "Franco", combined1["name"] );
+		Assert.areEqual( 26, combined1["age"] );
+		Assert.areEqual( "Haxe", combined1["language"] );
+
+		var combined2:TemplateData = [ dataObj, dataMap, { greatGuy: true } ];
+		Assert.areEqual( 4, Reflect.fields(combined2).length );
+		Assert.areEqual( true, combined2["greatGuy"] );
+
+		var combined3:TemplateData = [ dataObj, dataMap, ["greatGuy"=>true] ];
+		Assert.areEqual( 4, Reflect.fields(combined3).length );
+		Assert.areEqual( true, combined3["greatGuy"] );
+
+		var combined4:TemplateData = [ dataObj, dataMap, ["greatGuy"=>true] ];
+		Assert.areEqual( 4, Reflect.fields(combined4).length );
+		Assert.areEqual( true, combined4["greatGuy"] );
 	}
 }
