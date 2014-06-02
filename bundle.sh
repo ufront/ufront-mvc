4925ad26cc0bb6cef5b0ea4c2541b177882e0701
Jason O'Neil <jason.oneil@gmail.com>
2014-06-03 06:14:31 +0800

    Release bump

--- bundle.sh
+++ bundle.sh
@@ -3,4 +3,4 @@
 libname='ufront-mvc'
 rm -f "${libname}.zip"
 zip -r "${libname}.zip" haxelib.json src LICENSE.txt README.md
-echo "Saved as ${libname}.zip"
+echo "Saved as ${libname}.zip - please run a git tag"
