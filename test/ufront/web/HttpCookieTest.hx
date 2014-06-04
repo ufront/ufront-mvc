8248ade90b0a30c8dd3cea09ae199da4475a432a
Jason O'Neil <jason.oneil@gmail.com>
2014-06-04 12:03:09 +0800

    Unit tests for HttpCookie

--- test/ufront/web/HttpCookieTest.hx
+++ test/ufront/web/HttpCookieTest.hx
@@ -20,7 +20,21 @@ class HttpCookieTest
 	
 	public function teardown():Void {}
 	
-	public function testExample():Void {
-		Assert.fail( "Tests not written yet" );
+	public function testDescription() {
+		var cookie = new HttpCookie("name", "value");
+		Assert.equals("name", cookie.name);
+		Assert.equals("value", cookie.description());
+		
+		cookie.expires = Date.fromString("2001-01-01");
+		Assert.equals("value; expires=Mon, 01-Jan-2001 00:00:00 GMT", cookie.description());
+		
+		cookie.domain = "example.com";
+		Assert.equals("value; expires=Mon, 01-Jan-2001 00:00:00 GMT; domain=example.com", cookie.description());
+		
+		cookie.path = "/path";
+		Assert.equals("value; expires=Mon, 01-Jan-2001 00:00:00 GMT; domain=example.com; path=/path", cookie.description());
+		
+		cookie.secure = true;
+		Assert.equals("value; expires=Mon, 01-Jan-2001 00:00:00 GMT; domain=example.com; path=/path; secure", cookie.description());
 	}
 }
