3f6b75281fc9d815bccf45a1f2f0cb309abe658b
Jason O'Neil <jason.oneil@gmail.com>
2014-06-19 21:32:39 +0800

    Add tests for Message / MessageList

--- test/ufront/log/MessageTest.hx
+++ test/ufront/log/MessageTest.hx
@@ -5,8 +5,6 @@ import ufront.log.Message;
 
 class MessageTest 
 {
-	var instance:Message; 
-	
 	public function new() 
 	{
 		
@@ -20,6 +18,32 @@ class MessageTest
 	
 	public function teardown():Void {}
 	
-	public function testExample():Void {
+	public function testMessageList():Void {
+		var message1 = createMessage( "Hello Theo", Trace );
+		var message2 = createMessage( 400, Error );
+
+		var emptyMessageList = new MessageList();
+		emptyMessageList.push( message1 );
+		emptyMessageList.push( message2 );
+
+		var messageArray1 = [];
+		var messageArray2 = [];
+		function onMessageFn(m:Message) messageArray2.push(m);
+		var messageList = new MessageList( messageArray1, onMessageFn );
+		
+		Assert.equals( messageArray1, messageList.messages );
+		Assert.equals( onMessageFn, messageList.onMessage );
+		messageList.push( message1 );
+		Assert.equals( 1, messageArray1.length );
+		Assert.equals( 1, messageArray2.length );
+		messageList.push( message2 );
+		Assert.equals( 2, messageArray1.length );
+		Assert.equals( 2, messageArray2.length );
+		Assert.equals( "Hello Theo", messageArray1[0].msg );
+		Assert.equals( Error, messageArray2[1].type );
+	}
+
+	function createMessage( msg:Dynamic, type:MessageType, ?pos:haxe.PosInfos ) {
+		return { msg: msg, pos: pos, type: type };
 	}
 }
