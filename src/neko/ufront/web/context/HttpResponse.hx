555592752f3b7948c20a3c388d185da1b61067f2
Jason O'Neil <jason.oneil@gmail.com>
2014-10-04 21:18:21 +0800

    Use neko.Web instead of untyped neko modules for HttpResponse

--- src/neko/ufront/web/context/HttpResponse.hx
+++ src/neko/ufront/web/context/HttpResponse.hx
@@ -5,15 +5,13 @@
 
 package neko.ufront.web.context;
 
-import thx.sys.Lib;
 import thx.error.Error;
-
+import neko.Web;
 using StringTools;
 
 class HttpResponse extends ufront.web.context.HttpResponse {
 	public function new() {
 		super();
-		_init();
 	}
 
 	override function flush() {
@@ -23,18 +21,16 @@ class HttpResponse extends ufront.web.context.HttpResponse {
 		_flushed = true;
 		
 		// Set HTTP status code
-		_set_return_code( status );
+		Web.setReturnCode( status );
 
 		// Set Cookies
 		try {
 			for ( cookie in _cookies ) {
-				var description = cookie.description;
-				 var name = cookie.name;
-				_set_cookie( untyped name.__s, untyped description.__s );
+				Web.setCookie( cookie.name, cookie.value, cookie.expires, cookie.domain, cookie.path, cookie.secure, cookie.httpOnly );
 			}
 		}
 		catch ( e:Dynamic ) {
-			throw new Error( 'Cannot flush cookies on response, output already sent: $e' );
+			throw new Error( 'Cannot flush cookies on response: $e' );
 		}
 
 		// Write headers
@@ -44,7 +40,7 @@ class HttpResponse extends ufront.web.context.HttpResponse {
 				val += "; charset=" + charset;
 			}
 			try {
-				_set_header( untyped key.__s, untyped val.__s );
+				Web.setHeader( key, val );
 			}
 			catch ( e:Dynamic ) {
 				throw new Error( "Invalid header: '{0}: {1}', or output already sent", [key,val] );
@@ -52,23 +48,6 @@ class HttpResponse extends ufront.web.context.HttpResponse {
 		}
 
 		// Write response content
-		Lib.print( _buff.toString() );
-	}
-
-	static var _set_header : Dynamic;
-	static var _set_cookie : Dynamic;
-	static var _set_return_code : Dynamic;
-	static var _inited = false;
-
-	static function _init() {
-		if(_inited)
-			return;
-		_inited = true;
-		var get_env = Lib.load( "std", "get_env", 1 );
-		var ver = untyped get_env( "MOD_NEKO".__s );
-		var lib = "mod_neko" + if ( ver==untyped "1".__s ) "" else ver;
-		_set_header = Lib.load( lib, "set_header", 2 );
-		_set_cookie = Lib.load( lib, "set_cookie", 2 );
-		_set_return_code = Lib.load( lib, "set_return_code", 1 );
+		Sys.print( _buff.toString() );
 	}
 }
