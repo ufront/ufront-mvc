2b73bb1d2448e1790f83f5f1c0e9bb4d1d3c440d
Jason O'Neil <jason.oneil@gmail.com>
2014-10-25 11:36:34 +0800

    Add HttpAuthResult for basic HTTP auth...

---
+++ src/ufront/web/result/HttpAuthResult.hx
@@ -0,0 +1,47 @@
+package ufront.web.result;
+
+import ufront.web.result.ActionResult;
+import ufront.web.context.*;
+import ufront.web.HttpError;
+import tink.CoreApi;
+import ufront.core.Sync;
+
+
+/**
+	A result that requires a certain username and password to be provided before executing an action.
+**/
+class HttpAuthResult extends ActionResult {
+
+	var message:String;
+	var failureMessage:String;
+
+	/**
+		@param username The expected username.
+		@param password The expected password.
+		@param message (optional) The message to show in the popup box. Default is "Please login";
+		@param failureMessage (optional) The HTML to show in the browser if the login is cancelled. Default is to re-use `message`.s
+		@param fn The function to execute if authentication is correct. Must return a `FutureActionOutcome`, such as from using `executeSubController( AdminController )`.
+		@return A FutureActionOutcome, either the result of `fn()` or a `HttpAuthResult` that displays a login box.
+	**/
+	public static function require( context:HttpContext, username:String, password:String, ?message:String, ?failureMessage:String, fn:Void->FutureActionOutcome ):FutureActionOutcome {
+		var auth = context.request.authorization;
+		if ( auth!=null && auth.user==username && auth.pass==password ) {
+			return fn();
+		}
+		else {
+			var result:ActionResult = new HttpAuthResult(message,failureMessage);
+			return Future.sync( Success(result) );
+		}
+	}
+
+	function new( message:String, failureMessage:Null<String> ) {
+		this.message = message;
+		this.failureMessage = failureMessage!=null ? failureMessage : message;
+	}
+
+	override public function executeResult( actionContext:ActionContext ) {
+		actionContext.httpContext.response.requireAuthentication( message );
+		actionContext.httpContext.response.write( failureMessage );
+		return Sync.success();
+	}
+}
