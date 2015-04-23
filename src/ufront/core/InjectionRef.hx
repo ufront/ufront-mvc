45dd9c572826d407219f6fa2f4c3b1020a0da2d5
Jason O'Neil <jason.oneil@gmail.com>
2015-04-23 11:05:20 +0800

    Don't pool injection refs.
    That was always a dumb idea when request caching is a thing and your injections
    are used in multiple requests.

--- src/ufront/core/InjectionRef.hx
+++ src/ufront/core/InjectionRef.hx
@@ -22,21 +22,12 @@ package ufront.core;
 	```haxe
 	injector.mapValue( InjectionRef, "someInt", InjectionRef.of(99) );
 	```
-
-	Please note that all `InjectionRef` objects are recycled - after `get()` is called the value is emptied and the object is added to a pool to be used on the next `of()` call.
 **/
 class InjectionRef<T> {
 
-	static var pool:Array<InjectionRef<Dynamic>> = [];
-
 	/** Return a reference for the current value. **/
-	public static function of<T>( v:T ):InjectionRef<T> {
-		if ( pool.length>0 ) {
-			var r:InjectionRef<T> = cast pool.shift();
-			r.value = v;
-			return r;
-		}
-		else return new InjectionRef( v );
+	public static inline function of<T>( v:T ):InjectionRef<T> {
+		return new InjectionRef( v );
 	}
 
 	var value:T;
@@ -46,10 +37,7 @@ class InjectionRef<T> {
 		Return the value behind the current reference.
 		Please note that this can only be called once - after that the value is set to null and the object is recycled.
 	**/
-	public function get():T {
-		var v = value;
-		value = null;
-		pool.push( this );
-		return v;
+	public inline function get():T {
+		return value;
 	}
 }
