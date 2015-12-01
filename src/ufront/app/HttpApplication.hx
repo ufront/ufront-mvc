ufront/ufront-mvc

commit eae25dd9b2412b52a2c5fd67ca2fd6d630555d42

No longer passing errors through to express, as they're handled by ufront.
See discussion at:

https://gitter.im/ufront/ufront?at=5653ed29f2bb4538210af1fb

src/ufront/app/HttpApplication.hx
@@ -548,10 +548,7 @@ class HttpApplication
 			var context:HttpContext =
 				if ( pathToContentDir!=null ) HttpContext.createNodeJsContext( req, res, injector, urlFilters, pathToContentDir )
 				else HttpContext.createNodeJsContext( req, res, urlFilters );
-			this.execute( context ).handle( function(result) switch result {
-				case Failure( err ): next( new express.Error(err.toString()) );
-				default: next( null );
-			});
+			this.execute( context ).handle( function(result) next() );
 		};
 		app.use( ufAppMiddleware );
 		app.listen( port );

commit 0f8f3d75fcccc4d17e947ecefe2ad4cbbe4ecbc7

Set `err` as null.
eae25dd9b2412b52a2c5fd67ca2fd6d630555d42 accidentally broke compilation.

src/ufront/app/HttpApplication.hx
@@ -548,7 +548,7 @@ class HttpApplication
 			var context:HttpContext =
 				if ( pathToContentDir!=null ) HttpContext.createNodeJsContext( req, res, injector, urlFilters, pathToContentDir )
 				else HttpContext.createNodeJsContext( req, res, urlFilters );
-			this.execute( context ).handle( function(result) next() );
+			this.execute( context ).handle( function(result) next(null) );
 		};
 		app.use( ufAppMiddleware );
 		app.listen( port );